c7bd715138964ebe2fe8dcb56ac5598daea481bd
Guilherme Vale <guilherme.vale98@gmail.com>
2024-08-15 01:39:27 -0300

    add services component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeHeroComponent } from "../home-hero/home-hero.component";
+import { ServicesComponent } from "../services/services.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeHeroComponent],
+  imports: [HomeHeroComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
