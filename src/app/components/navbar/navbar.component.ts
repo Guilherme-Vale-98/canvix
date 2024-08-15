faa7e44ba06b3a1a3788434bd551d662b419b3cf
Guilherme Vale <guilherme.vale98@gmail.com>
2024-08-15 15:46:28 -0300

    add navbar animation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,8 +7,33 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  whiteBg = false;
+  logoUrl = "assets/logo.png";
+  
+  ngOnInit() {
+    this.checkScrollPosition();
+  }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkScrollPosition();
+  }
+
+  checkScrollPosition() {
+    if (typeof window !== "undefined"){
+      const scrollPosition =
+      window.scrollY ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    
+    this.whiteBg = scrollPosition > 680 && scrollPosition < 1200 ;
+  
+    this.logoUrl = scrollPosition > 680 ? "assets/logoBlack.png" : "assets/logo.png";
+  
+  }
+  }
 }
