220bdd9f0d34fe4caa867a4283663d8a5adfa3ae
Guilherme Vale <guilherme.vale98@gmail.com>
2024-08-21 14:37:04 -0300

    add showcase component

---
+++ src/app/components/project-card/project-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Project } from './project-card.models';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-project-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './project-card.component.html',
+  styleUrl: './project-card.component.css'
+})
+export class ProjectCardComponent {
+
+  @Input() project!: Project;
+
+}
--- src/app/components/showcase/showcase.component.ts
+++ src/app/components/showcase/showcase.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { ProjectCardComponent } from "../project-card/project-card.component";
+import { CommonModule } from '@angular/common';
+import { Project } from '../project-card/project-card.models';
 
 @Component({
   selector: 'app-showcase',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProjectCardComponent],
   templateUrl: './showcase.component.html',
   styleUrl: './showcase.component.css'
 })
 export class ShowcaseComponent {
+projects: Project[] = [
+  {
+    imgUrl: "assets/web ui design.png",
+    title: "Web UI design",
+    subtitle: "Creative UI design"
+  }
+  ,{
+    imgUrl: "assets/digital strategy.png",
+    title: "To design Digital Strategy",
+    subtitle: "Social Media Marketing"
+  }
+  ,{
+    imgUrl: "assets/UI design 2.png",
+    title: "To design Digital Strategy",
+    subtitle: "Social Media Marketing"
+  }
+  ,{
+    imgUrl: "assets/UI design.png",
+    title: "To design Digital Strategy",
+    subtitle: "Social Media Marketing"
+  }
+];
 
 }
