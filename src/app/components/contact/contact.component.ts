0800e33d0817fa891902f56e8e82b921e3c04160
Guilherme Vale <guilherme.vale98@gmail.com>
2024-08-23 15:08:15 -0300

    add contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherInstagram, featherFacebook, featherLinkedin } from '@ng-icons/feather-icons';
+import { tablerMail, tablerMapPin, tablerPhone } from '@ng-icons/tabler-icons';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({tablerMail, tablerPhone, tablerMapPin, featherInstagram, featherFacebook, featherLinkedin})],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
