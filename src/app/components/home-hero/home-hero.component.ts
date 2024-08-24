4847eca93559b189036e35074747813b52a06b00
Guilherme Vale <guilherme.vale98@gmail.com>
2024-08-24 02:28:46 -0300

    add overlay for loading home

--- src/app/components/home-hero/home-hero.component.ts
+++ src/app/components/home-hero/home-hero.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-hero.component.html',
   styleUrl: './home-hero.component.css'
 })
-export class HomeHeroComponent {
+export class HomeHeroComponent implements OnInit {
+  imagesLoaded: boolean = false;
 
+  constructor() {}
+
+  ngOnInit(): void {
+   if(typeof window !== "undefined") this.preloadImages();
+  }
+
+  preloadImages(): void {
+    const images = [
+      'assets/ornament1.png',
+      'assets/homeHeroImage.png',
+      'assets/brands1.png'
+    ];
+
+    let imagesLoadedCount = 0;
+    const totalImages = images.length;
+
+    images.forEach(src => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => {
+        imagesLoadedCount++;
+        if (imagesLoadedCount === totalImages) {
+          this.imagesLoaded = true;
+        }
+      };
+    });
+  }
 }
